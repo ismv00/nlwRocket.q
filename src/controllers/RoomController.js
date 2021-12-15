29c39e384155c5c2806b1b57d84b6c6392cc4d37
Igor Menezes <ismv00@icloud.com>
2021-12-15 19:44:51 -0400

    Aula05 - final

--- src/controllers/RoomController.js
+++ src/controllers/RoomController.js
@@ -1,29 +1,60 @@
+
 const Database = require('../db/config');
 
 module.exports = {
-    async create(req, res){
+    async create(req, res) {
 
         const db = await Database()
         const pass = req.body.password
-        let roomId ;
+        let roomId;
+        let isRoom = true;
+        while (isRoom) {
+            //gera o número da sala
+            for (var i = 0; i < 6; i++) {
+                i == 0 ? roomId = Math.floor(Math.random() * 10).toString() :
+                    roomId += Math.floor(Math.random() * 10).toString()
+            }
+            //verificar se o numero da sala existe
+            const roomsExistIds = await db.all(`SELECT id  FROM rooms `)
+            isRoom = roomsExistIds.some(roomsExistId => roomsExistId === roomId);
 
-        for( var i = 0; i < 6; i++ ){
-            i == 0 ? roomId = Math.floor(Math.random() * 10).toString() : 
-            roomId += Math.floor(Math.random() * 10).toString()
+
+            if (!isRoom) {
+                await db.run(`INSERT INTO rooms (
+                id,
+                pass
+            ) VALUES (
+                ${parseInt(roomId)},
+                "${pass}"
+            )`)
         }
-        
+    }
+    
+    
+    await db.close();
+    res.redirect(`/room/${roomId}`)
 
-        await db.run(`INSERT INTO rooms (
-            id,
-            pass
-        ) VALUES (
-            ${parseInt(roomId)},
-            ${pass}
-        )`)
 
-        await db.close();
+    },
 
-        
+    async open(req, res){
+        const db = await Database();
+        const roomId = req.params.room;        
+        const questions = await db.all(`SELECT * FROM questions WHERE room = ${roomId} AND read = 0`);
+        const questionsRead = await db.all(`SELECT * FROM questions WHERE room = ${roomId} AND read = 1`)
+
+        let isNoQuestions
+
+        if(questions.length == 0) {
+            if(questionsRead.length == 0 ){
+                isNoQuestions = true;
+            }
+        }
+        res.render('room', {roomId: roomId, questions: questions, questionsRead, isNoQuestions: isNoQuestions})
+    },
+    enter(req, res){
+        const roomId = req.body.roomId
+
         res.redirect(`/room/${roomId}`)
     }
 }
